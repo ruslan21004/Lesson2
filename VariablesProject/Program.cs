9c399ed323ae136e12e62e1c870dca19eba638c7
ruslan21004 <ruslan21004@gmail.com>
2022-08-01 20:22:15 +0300

    thirdcommit

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -1,11 +1,19 @@
-﻿// double number = null; 
+﻿Console.WriteLine("Hello world");
 
-// string str = null;
+Console.WriteLine("Hello world");
 
-// int? nullInt = null;
+Console.WriteLine("Hello world");
+Console.WriteLine("Hello world");
+Console.WriteLine("Hello world");
 
-int variable;
-variable = 5;
 
-Console.WriteLine(variable);
+Console.WriteLine("Hello world");
+
+Console.WriteLine("Hello world");
+
+Console.WriteLine("Hello world");
+Console.WriteLine("Hello world");
+Console.WriteLine("Hello world");
+
+Console.WriteLine("Hello world");
 
