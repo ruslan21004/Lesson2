5ad4adfb5f91714b9fea372d5146562d2fad8211
ruslan21004 <ruslan21004@gmail.com>
2022-08-02 13:57:47 +0300

    firstmathcommit

--- VariablesProject/Program.cs
+++ VariablesProject/Program.cs
@@ -1,29 +1,41 @@
-﻿Console.WriteLine("Hello world");
+﻿
+//int x = 100 / 50;
+//int sum1 = 100 + 50;
+//int sum2 = sum1 + 250;
+//int sum3 = sum2 + sum1;
+//sum3++;
+//sum3 = sum3 + 1;
+//sum3--;
+//sum3 = sum3 - 1;
+//x = 5;
+//x += 5;
+//x = x + 5;
+//x -= 5;
+//x = x - 5;
+//x *= 5;
+//x = x * 5;
+//x /= 5;
+//x = x / 5;
 
-Console.WriteLine("Hello world");
+//-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15
+int num = -6;
 
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
+double xx = -6 * Math.Pow(num, 3) + 5 * Math.Pow(num, 2) - 10 * num + 15;
+Console.WriteLine(xx);
 
+int num1 = 5;
+double xxx = Math.Abs(num) * Math.Sin(num);
+Console.WriteLine(xxx);
 
-Console.WriteLine("Hello world");
+//2 * pi * x
 
-Console.WriteLine("Hello world");
+int num2 = -6;
 
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
+double xxx2 = 2 * Math.PI * num2;
+Console.WriteLine(xxx2);
 
-Console.WriteLine("Hello world");
+int num3 = 32;
+int num4 = -6;
+double xxx3 = Math.Max(num3, num4);
+Console.WriteLine(xxx3);
 
-Console.WriteLine("Hello world");
-
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
-
-Console.WriteLine("Hello world");
-Console.WriteLine("Hello world");
